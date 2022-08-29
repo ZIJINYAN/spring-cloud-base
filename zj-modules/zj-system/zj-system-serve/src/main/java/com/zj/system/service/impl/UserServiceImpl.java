a2767d502db2494a65b312c839ea3b3fe7fce0b2
ZiJin <zijincyan@outlook.com>
2022-08-29 23:31:54 +0800

    整合rabbit mq

--- zj-modules/zj-system/zj-system-serve/src/main/java/com/zj/system/service/impl/UserServiceImpl.java
+++ zj-modules/zj-system/zj-system-serve/src/main/java/com/zj/system/service/impl/UserServiceImpl.java
@@ -1,11 +1,14 @@
 package com.zj.system.service.impl;
 
+import com.alibaba.fastjson.JSONObject;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.zj.common.core.domain.Result;
 import com.zj.system.common.domain.UserEntity;
 import com.zj.system.mapper.UserMapper;
 import com.zj.system.service.IUserService;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
@@ -14,8 +17,13 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, UserEntity> implements IUserService {
+
+    @Autowired
+    private RabbitTemplate rabbitTemplate;
+
     @Override
     public Result<UserEntity> login(UserEntity userLogin) {
+        rabbitTemplate.convertAndSend("system.user.direct.exchange","system.user", JSONObject.toJSONString(userLogin));
         UserEntity userEntity = this.baseMapper.selectOne(
                 new QueryWrapper<UserEntity>().lambda()
                         .eq(UserEntity::getUsername, userLogin.getUsername())
